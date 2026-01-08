dea011be85da6fa66332cc27708503d1fc7fe17b
Angel Díaz <angeldiaz.hrn@gmail.com>
2026-01-08 14:32:21 -0600

    Inicio proyecto

---
+++ src/header.tsx
@@ -0,0 +1,9 @@
+export const Header = () => {
+  return (
+    <header className="grid grid-cols-2 items-center">
+      <img src="path/to/image.jpg" alt="Angel Díaz" />
+      <h1 className="text-3xl">Angel Ignacio Díaz Hernández</h1>
+      <h2>Administrador de Bases de Datos Junior | Estudiante de TI.</h2>
+    </header>
+  );
+};
