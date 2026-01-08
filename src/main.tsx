25449f2a2327fde9f459f5d2e1c5d5e3bf9ff2e5
Angel Díaz <angeldiaz.hrn@gmail.com>
2026-01-08 16:04:28 -0600

    Instalación de fuente

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,9 @@ import { Content } from "./content";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <Header />
-    <Content />
+    <div className="font-poppins min-h-screen space-y-5 px-80 py-5">
+      <Header />
+      <Content />
+    </div>
   </StrictMode>,
 );
