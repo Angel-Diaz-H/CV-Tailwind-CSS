89a5e5e402b84e5eab7490594a50694193fc5d3b
Angel Díaz <angeldiaz.hrn@gmail.com>
2026-01-12 14:36:34 -0600

    DR capacitaciones y tecnologías

--- src/content.tsx
+++ src/content.tsx
@@ -12,7 +12,7 @@ export const Content = () => {
       <ResumenProfesional />
       <ExperienciaProfesional />
       <Educacion />
-      <div className="grid grid-cols-2 space-x-5">
+      <div className="grid grid-cols-1 space-y-5 space-x-5 text-justify sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-2">
         <Capacitaciones />
         <TecnologiasHabilidades />
       </div>
