cb3846d4307c82a21a92cb23a633e2ec41a18294
Angel Díaz <angeldiaz.hrn@gmail.com>
2026-01-15 17:15:05 -0600

    Font-medium en capacitaciones

--- src/components/Capacitaciones.tsx
+++ src/components/Capacitaciones.tsx
@@ -8,43 +8,47 @@ export const Capacitaciones = () => {
           <ul className="ml-2 list-outside space-y-1 py-1">
             <li>
               <a
+                className="font-medium"
                 href="https://cursos.devtalles.com/certificates/7iiruz355a"
                 target="_blank"
                 rel="noreferrer"
               >
                 Tailwind CSS: Para desarrolladores de software.
-                <p>Enero 2026.</p>
               </a>
+              <p>Enero 2026.</p>
             </li>
             <li>
               <a
+                className="font-medium"
                 href="https://cursos.devtalles.com/certificates/ijmiuxrlkz"
                 target="_blank"
                 rel="noreferrer"
               >
                 TypeScript: Tu completa guía y manual de mano.
-                <p>Noviembre 2025.</p>
               </a>
+              <p>Noviembre 2025.</p>
             </li>
             <li>
               <a
+                className="font-medium"
                 href="https://cursos.devtalles.com/certificates/oexl95fk8x"
                 target="_blank"
                 rel="noreferrer"
               >
                 JavaScript Moderno: Guia para dominar el lenguaje.
-                <p>Octubre 2025.</p>
               </a>
+              <p>Octubre 2025.</p>
             </li>
             <li>
               <a
+                className="font-medium"
                 href="https://cursos.devtalles.com/certificates/oym8nqbqvz"
                 target="_blank"
                 rel="noreferrer"
               >
                 Programacion para principiantes - Primeros.
-                <p>Septiembre 2025.</p>
               </a>
+              <p>Septiembre 2025.</p>
             </li>
           </ul>
         </div>
@@ -54,23 +58,25 @@ export const Capacitaciones = () => {
           <ul className="ml-2 list-outside space-y-1 py-1">
             <li>
               <a
+                className="font-medium"
                 href="https://ple.jetro.ws/certificate-validation?credentialId=XSEBQAMYPNXMS"
                 target="_blank"
                 rel="noreferrer"
               >
                 DE. 102. Aplicando FODA.
-                <p>Agosto 2025.</p>
               </a>
+              <p>Agosto 2025.</p>
             </li>
             <li>
               <a
+                className="font-medium"
                 href="https://ple.jetro.ws/certificate-validation?credentialId=ULX3US6VJ6782"
                 target="_blank"
                 rel="noreferrer"
               >
                 DE. 103. Abordando Riesgos y Oportunidades.
-                <p>Agosto 2025.</p>
               </a>
+              <p>Agosto 2025.</p>
             </li>
           </ul>
         </div>
@@ -80,13 +86,14 @@ export const Capacitaciones = () => {
           <ul className="ml-2 list-outside space-y-1 py-1">
             <li>
               <a
+                className="font-medium"
                 href="https://www.linkedin.com/in/angeldiaz-hrn/details/certifications/1749512261084/single-media-viewer/?profileId=ACoAAD1Nz2MBOJq7tgoDIw5pIcX7GbvPbC4PS30"
                 target="_blank"
                 rel="noreferrer"
               >
                 Data Analytics with POWER BI.
-                <p>Mayo 2025.</p>
               </a>
+              <p>Mayo 2025.</p>
             </li>
           </ul>
         </div>
