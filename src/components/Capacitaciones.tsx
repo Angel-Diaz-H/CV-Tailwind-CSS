17139a2c00d9c6823c3ab289a40e6db20bb15c8b
Angel Díaz <angeldiaz.hrn@gmail.com>
2026-01-15 17:11:11 -0600

    URL de cursos

--- src/components/Capacitaciones.tsx
+++ src/components/Capacitaciones.tsx
@@ -7,28 +7,44 @@ export const Capacitaciones = () => {
           <h3 className="font-bold">DevTalles.</h3>
           <ul className="ml-2 list-outside space-y-1 py-1">
             <li>
-              <p className="font-medium">
+              <a
+                href="https://cursos.devtalles.com/certificates/7iiruz355a"
+                target="_blank"
+                rel="noreferrer"
+              >
                 Tailwind CSS: Para desarrolladores de software.
-              </p>
-              <p>Enero 2026.</p>
+                <p>Enero 2026.</p>
+              </a>
             </li>
             <li>
-              <p className="font-medium">
+              <a
+                href="https://cursos.devtalles.com/certificates/ijmiuxrlkz"
+                target="_blank"
+                rel="noreferrer"
+              >
                 TypeScript: Tu completa guía y manual de mano.
-              </p>
-              <p>Noviembre 2025.</p>
+                <p>Noviembre 2025.</p>
+              </a>
             </li>
             <li>
-              <p className="font-medium">
+              <a
+                href="https://cursos.devtalles.com/certificates/oexl95fk8x"
+                target="_blank"
+                rel="noreferrer"
+              >
                 JavaScript Moderno: Guia para dominar el lenguaje.
-              </p>
-              <p>Octubre 2025.</p>
+                <p>Octubre 2025.</p>
+              </a>
             </li>
             <li>
-              <p className="font-medium">
+              <a
+                href="https://cursos.devtalles.com/certificates/oym8nqbqvz"
+                target="_blank"
+                rel="noreferrer"
+              >
                 Programacion para principiantes - Primeros.
-              </p>
-              <p>Septiembre 2025.</p>
+                <p>Septiembre 2025.</p>
+              </a>
             </li>
           </ul>
         </div>
@@ -37,14 +53,24 @@ export const Capacitaciones = () => {
           <h3 className="font-bold">Jetro Systemic.</h3>
           <ul className="ml-2 list-outside space-y-1 py-1">
             <li>
-              <p className="font-medium">DE. 102. Aplicando FODA.</p>
-              <p>Agosto 2025.</p>
+              <a
+                href="https://ple.jetro.ws/certificate-validation?credentialId=XSEBQAMYPNXMS"
+                target="_blank"
+                rel="noreferrer"
+              >
+                DE. 102. Aplicando FODA.
+                <p>Agosto 2025.</p>
+              </a>
             </li>
             <li>
-              <p className="font-medium">
+              <a
+                href="https://ple.jetro.ws/certificate-validation?credentialId=ULX3US6VJ6782"
+                target="_blank"
+                rel="noreferrer"
+              >
                 DE. 103. Abordando Riesgos y Oportunidades.
-              </p>
-              <p>Agosto 2025.</p>
+                <p>Agosto 2025.</p>
+              </a>
             </li>
           </ul>
         </div>
@@ -53,8 +79,14 @@ export const Capacitaciones = () => {
           <h3 className="font-bold">Desitel Centro de Capacitaciones.</h3>
           <ul className="ml-2 list-outside space-y-1 py-1">
             <li>
-              <p className="font-medium">Data Analytics with POWER BI.</p>
-              <p>Mayo 2025.</p>
+              <a
+                href="https://www.linkedin.com/in/angeldiaz-hrn/details/certifications/1749512261084/single-media-viewer/?profileId=ACoAAD1Nz2MBOJq7tgoDIw5pIcX7GbvPbC4PS30"
+                target="_blank"
+                rel="noreferrer"
+              >
+                Data Analytics with POWER BI.
+                <p>Mayo 2025.</p>
+              </a>
             </li>
           </ul>
         </div>
